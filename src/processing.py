e577297a71c3b844a370709e4dcdf4d4c26a3d5b
rolvs <ratova949@gmail.com>
2025-09-03 09:18:01 +0300

    run linting and autoformatting

--- src/processing.py
+++ src/processing.py
@@ -1,12 +1,11 @@
-from typing import List, Dict
-
-def filter_by_state(items: List[Dict], state: str = "EXECUTED") -> List[Dict]:
-    """Ф-ия возвращает список словарей, у которых ключ state соответствует указанному значению. """
-    return [item for item in items if item.get('state') == state]
+from typing import Dict, List, Any
 
 
-from typing import List, Dict
+def filter_by_state(items: List[Dict[str, Any]], state: str = "EXECUTED") -> List[Dict[str, Any]]:
+    """Ф-ия возвращает список словарей, у которых ключ state соответствует указанному значению."""
+    return [item for item in items if item.get("state") == state]
 
-def sort_by_date(items: List[Dict], descending: bool = True) -> List[Dict]:
+
+def sort_by_date(items: List[Dict[str, Any]], descending: bool = True) -> List[Dict[str, Any]]:
     """Ф-ия возвращает новый список, отсортированный по дате"""
-    return sorted(items, key=lambda x: x['date'], reverse=descending)
+    return sorted(items, key=lambda x: x["date"], reverse=descending)
